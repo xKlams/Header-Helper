05e1d8e2d4f9e9d78263845d3f6fcd71622502d9
Federico De Sisti <federicodesisti@gmail.com>
2024-08-23 00:41:29 +0200

    bug fixes

--- monitor.sh
+++ monitor.sh
@@ -6,8 +6,8 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 inotifywait -m -e modify,create,delete "$DIRECTORY_TO_MONITOR" |
 while read path action file; do
-    # Perform your action here
     if [ "/$file" != "$HEADER_FILE" ]; then
+	echo "$file"
     	python3 "$SCRIPT_DIR"/header_updater.py $DIRECTORY_TO_MONITOR $HEADER_FILE
     fi
 done
